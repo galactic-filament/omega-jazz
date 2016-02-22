a61b7d496776843b5197bb435de8c4d25ab164a8
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-02-21 23:49:13 -0500

    Add missing content-type header.

--- app/lib/posts_routes.rb
+++ app/lib/posts_routes.rb
@@ -7,20 +7,28 @@ end
 
 class PostsRoutes < Sinatra::Base
   post '/posts' do
+    content_type :json
+
     request_body = JSON.parse request.body.read
     post = Post.create(body: request_body['body'])
     post.to_json
   end
 
   get '/post/:id' do
+    content_type :json
+
     Post.find(params['id']).to_json
   end
 
   delete '/post/:id' do
+    content_type :json
+
     Post.destroy(params['id'])
   end
 
   put '/post/:id' do
+    content_type :json
+
     request_body = JSON.parse request.body.read
     post = Post.find(params['id'])
     post.update(body: request_body['body'])
