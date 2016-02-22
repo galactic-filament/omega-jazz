af0066c154a997f5654b565f40b587d2258d8f4b
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-02-21 22:38:02 -0500

    Remove explicit error logging.

--- app/lib/posts_routes.rb
+++ app/lib/posts_routes.rb
@@ -6,8 +6,6 @@ class Post < ActiveRecord::Base
 end
 
 class PostsRoutes < Sinatra::Base
-  enable :logging, :dump_errors, :raise_errors
-
   post '/posts' do
     request_body = JSON.parse request.body.read
     post = Post.create(body: request_body['body'])
