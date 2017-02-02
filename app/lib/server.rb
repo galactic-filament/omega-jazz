27c27629c464560e64ebeb971e4a5399e6ffe122
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-02-01 20:33:59 -0500

    Switch to logging into app.log.

--- app/lib/server.rb
+++ app/lib/server.rb
@@ -11,7 +11,7 @@ class Server < Sinatra::Base
   ::Logger.class_eval { alias :write :'<<' }
 
   # access logging
-  access_logger = ::Logger.new(::File.join(ENV['APP_LOG_DIR'], 'access.log'))
+  access_logger = ::Logger.new(::File.join(ENV['APP_LOG_DIR'], 'app.log'))
   configure do
     use ::Rack::CommonLogger, access_logger
   end
