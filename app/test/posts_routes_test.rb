06b009433b0efd58e58e6abec3a64b90b5632995
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-02 20:34:38 -0500

    Clean up function alls.

--- app/test/posts_routes_test.rb
+++ app/test/posts_routes_test.rb
@@ -13,8 +13,8 @@ class PostRoutesTest < MiniTest::Test
   end
 
   def test_post_create
-    body = { body: 'Hello, world!' }
-    post '/posts', body.to_json
+    post '/posts', {body: 'Hello, world!'}.to_json
+
     assert_equal last_response.status, 201
 
     response_body = JSON.parse last_response.body
@@ -22,43 +22,39 @@ class PostRoutesTest < MiniTest::Test
   end
 
   def test_post_create_erroneous
-    body = {}
-    post '/posts', body.to_json
+    post '/posts', {}.to_json
+
     assert_equal last_response.status, 400
   end
 
   def test_post_get
-    # creating the post
-    body = { body: 'Hello, world!' }
-    create_response_body = _create_post body
+    create_response_body = _create_post({body: 'Hello, world!'})
 
-    # fetching it
-    id = create_response_body['id']
-    get_response_body = _test_get_json "/post/#{id}"
+    get "/post/#{create_response_body['id']}"
+
+    assert_equal last_response.status, 200
+
+    get_response_body = JSON.parse last_response.body
     assert_equal create_response_body['body'], get_response_body['body']
   end
 
   def test_post_delete
-    # creating the post
-    body = { body: 'Hello, world!' }
-    create_response_body = _create_post body
+    create_response_body = _create_post({body: 'Hello, world!'})
 
-    # deleting it
-    id = create_response_body['id']
-    delete "/post/#{id}"
+    delete "/post/#{create_response_body['id']}"
 
     assert last_response.ok?
   end
 
   def test_post_put
-    # creating the post
-    create_body = { body: 'Hello, world!' }
-    create_response_body = _create_post create_body
+    create_response_body = _create_post({body: 'Hello, world!'})
 
-    # updating it
-    id = create_response_body['id']
-    url = "/post/#{id}"
-    put_body = { body: 'Jello, world!' }
-    _test_put_json url, put_body
+    put_request_body = {body: 'Jello, world!'}
+    put "/post/#{create_response_body['id']}", put_request_body.to_json
+
+    assert last_response.ok?
+
+    put_response_body = JSON.parse last_response.body
+    assert_equal put_response_body['body'], put_response_body['body']
   end
 end
