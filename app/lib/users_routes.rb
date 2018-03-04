4f0e7c9d31764a3bd5a4ec72595a40f9520b6e6f
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-03 20:15:30 -0500

    Add routes, tests for user CRUD.

--- app/lib/users_routes.rb
+++ app/lib/users_routes.rb
@@ -40,4 +40,27 @@ class UsersRoutes < Base
     user.save!
     user.to_json only: [:id, :email]
   end
+
+  get '/user/:id' do
+    content_type :json
+
+    User.find(params['id']).to_json only: [:id, :email]
+  end
+
+  delete '/user/:id' do
+    content_type :json
+
+    User.destroy(params['id'])
+  end
+
+  put '/user/:id' do
+    content_type :json
+
+    parsed_body = JSON.parse(request.body.read)
+    user = User.find params['id']
+    user.email = parsed_body['email']
+    user.password = parsed_body['password']
+    user.save!
+    user.to_json only: [:id, :email]
+  end
 end
--- app/test/user_routes_test.rb
+++ app/test/user_routes_test.rb
@@ -22,4 +22,35 @@ class UserRoutesTest < MiniTest::Test
     assert response_body['id'].is_a? Numeric
     assert request_body[:email], response_body['email']
   end
+
+  def test_user_get
+    create_response_body = _create_user email: 'a@a.a', password: 'test'
+
+    get "/user/#{create_response_body['id']}"
+
+    assert_equal last_response.status, 200
+
+    get_response_body = JSON.parse last_response.body
+    assert_equal create_response_body['email'], get_response_body['email']
+  end
+
+  def test_user_delete
+    create_response_body = _create_user email: 'a@a.a', password: 'test'
+
+    delete "/user/#{create_response_body['id']}"
+
+    assert last_response.ok?
+  end
+
+  def test_user_put
+    create_response_body = _create_user email: 'a@a.a', password: 'test2'
+
+    put_request_body = {email: 'b@b.b'}
+    put "/user/#{create_response_body['id']}", put_request_body.to_json
+
+    assert_equal last_response.status, 200
+
+    put_response_body = JSON.parse last_response.body
+    assert_equal put_request_body[:email], put_response_body['email']
+  end
 end
