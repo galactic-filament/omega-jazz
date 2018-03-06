b54636808d18a3d33cb288601743e3211ca5b374
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-06 13:22:23 -0500

    Add missing routes.

--- app/lib/comments_routes.rb
+++ app/lib/comments_routes.rb
@@ -9,7 +9,7 @@ class CommentsRoutes < Base
 
     content_type :json
 
-    post = Post.find(params['id'])
+    post = Post.find_by_id params['id']
     if post == nil
       status 404
 
@@ -23,6 +23,52 @@ class CommentsRoutes < Base
     })
 
     status 201
+    comment.to_json
+  end
+
+  get '/comment/:id' do
+    env['warden'].authenticate!
+
+    content_type :json
+
+    comment = Comment.find_by id: params['id'], user: env['warden'].user
+    if comment == nil
+      status 404
+
+      return {}.to_json
+    end
+
+    comment.to_json
+  end
+
+  delete '/comment/:id' do
+    env['warden'].authenticate!
+
+    content_type :json
+
+    comment = Comment.find_by id: params['id'], user: env['warden'].user
+    if comment == nil
+      status 404
+
+      return {}.to_json
+    end
+
+    comment.destroy
+  end
+
+  put '/comment/:id' do
+    env['warden'].authenticate!
+
+    content_type :json
+
+    comment = Comment.find_by id: params['id'], user: env['warden'].user
+    if comment == nil
+      status 404
+
+      return {}.to_json
+    end
+
+    comment.update body: JSON.parse(request.body.read)['body']
     comment.to_json
   end
 end
